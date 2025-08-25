e1281e5455323aaadfb94007606dc659c5f0657c
nlonghorne <nlonghorne@gmail.com>
2025-08-25 22:35:24 +1000

    chore:initial commit

---
+++ src/main.py
@@ -0,0 +1,108 @@
+import os
+
+import requests
+import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+API_KEY = os.getenv("API_KEY")
+REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
+
+# Cached ID token and expiry
+id_token = None
+expiry_time = 0
+
+def send_email(subject, body):
+    msg = MIMEMultipart()
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = EMAIL_ADDRESS
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(body, "plain"))
+
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg.as_string())
+
+def get_id_token():
+    global id_token, expiry_time
+
+    # If token still valid, reuse it
+    if id_token and time.time() < expiry_time:
+        return id_token
+
+    url = f"https://securetoken.googleapis.com/v1/token?key={API_KEY}"
+    payload = {
+        "grant_type": "refresh_token",
+        "refresh_token": REFRESH_TOKEN
+    }
+
+    res = requests.post(url, data=payload)
+    res.raise_for_status()
+    ticket_data = res.json()
+
+    # Save ID token + expiry
+    id_token = ticket_data["id_token"]
+    expires_in = int(ticket_data["expires_in"])  # usually 3600
+    expiry_time = time.time() + expires_in - 60  # refresh 1 min early
+
+    return id_token
+
+
+def call_resale_api():
+    token = get_id_token()
+    url = "https://marketplace.ticketek.com.au/search/api/products"
+
+    params = {
+        "content_id": "TESSCG0126",
+        "sort": "date asc"
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    resp = requests.get(url, headers=headers, params=params)
+    resp.raise_for_status()
+    ticket_data = resp.json()
+
+    # Define the target dates
+    target_dates = {"2026-01-04", "2026-01-05", "2026-01-06", "2026-01-07"}
+
+    # Filter tickets by date
+    filtered = []
+    for product in ticket_data.get("results", []):
+        if product.get("available_tickets_count") > 0:
+            product_fields = product.get("fields")
+            date = product_fields.get("date")
+            if date:
+                date_only = date.split("T")[0]  # keep just YYYY-MM-DD
+                if date_only in target_dates:
+                    filtered.append(product)
+
+    email_text = "Dear Nat,\n\nAshes Tickets have been found for the following days:\n"
+
+    if len(filtered):
+        for ticket in filtered:
+            email_text += ticket.get("fields").get("when")
+            email_text += "\n"
+        email_text += "\nCheck here now:\nhttps://marketplace.ticketek.com.au/purchase/searchlist/products?keyword=ashes%20sydney&content_id=TESSCG0126"
+        email_text += "\n\nLove from,\nYourself <3"
+        send_email("Ashes Tickets Found!!", email_text)
+
+    if resp.status_code != 200:
+        err_msg = "Dear Nat,\n\nYour ticket script has an error with the request status code, take a look."
+        err_msg += f"\n\nStatus Code:\n{resp.status_code}\n\nFrom,\nYourself"
+        send_email("Oops there's a problem...", err_msg)
+
+
+# Example usage:
+if __name__ == "__main__":
+    call_resale_api()
